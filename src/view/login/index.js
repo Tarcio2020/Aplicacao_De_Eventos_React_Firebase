54b5c7fe4012456bb76e3d6f1db56f0af907ca18
Tarcio Teles <tarcioapps7@gmail.com>
2023-09-05 00:21:07 -0300

    Área de Login-parte 1

--- src/view/login/index.js
+++ src/view/login/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import './login.css';
 
 function Login() {
     return(
-        <h1>Login</h1>
+        <div className='login-content d-flex align-items-center'>
+            <form className='form-signin mx-auto'>
+                <h1 class="h3 mb-3 fw-normal text-white font-weigth-bold">Login</h1>
+                <input type="email" class="form-control my-2" id="floatingInput" placeholder="Email"/>             
+                <input type="password" class="form-control my-2" id="floatingPassword" placeholder="Senha"/>
+                <button class="btn btn-login w-100 py-2" type="submit">Sign in</button>
+
+                <p></p>
+            </form>
+        </div>
     )
 }
 
