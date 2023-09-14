24c58c7550831a26743b755239b51a6f124824a9
Tarcio Teles <tarcioapps7@gmail.com>
2023-09-13 23:29:21 -0300

    add

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -6,19 +6,17 @@ import { Link  } from 'react-router-dom';
 function Navbar() {
     return(
         <nav className="navbar navbar-expand-lg ">
-            <div className="container-fluid">
-                <span className="navbar-brand text-white text-weight-bold">Eventos</span>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                     </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
+            <span className="navbar-brand text-white font-weight-bold">Eventos</span>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fas fa-bars text-white"></i>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item"><Link className="nav-link" aria-current="page" to='/'>Home</Link></li>
-                    <li className="nav-item"><Link className="nav-link" aria-current="page" to='/novousuario'>Cadastrar</Link></li>
+                    <li className="nav-item"><Link className="nav-link" aria-current="page" to='/NovoUsuario'>Cadastrar</Link></li>
                     <li className="nav-item"><Link className="nav-link" aria-current="page" to='/login'>Login</Link></li>
 
                 </ul>
-                </div>
             </div>
         </nav>    
     )
