aa877935bd9808f46d13d3ad9dc605c31fdc7e1d
Tarcio Teles <tarcioapps7@gmail.com>
2023-09-14 23:38:14 -0300

    email de recuperação de senha

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -13,8 +13,17 @@ function Navbar() {
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item"><Link className="nav-link" aria-current="page" to='/'>Home</Link></li>
+                   
+                   <>
                     <li className="nav-item"><Link className="nav-link" aria-current="page" to='/NovoUsuario'>Cadastrar</Link></li>
                     <li className="nav-item"><Link className="nav-link" aria-current="page" to='/login'>Login</Link></li>
+                   </>
+
+                   <>
+                    <li className="nav-item"><Link className="nav-link" aria-current="page" to='/NovoUsuario'>Publicar Eventos</Link></li>
+                    <li className="nav-item"><Link className="nav-link" aria-current="page" to='/NovoUsuario'>Meus Eventos</Link></li>
+                    <li className="nav-item"><Link className="nav-link" aria-current="page" to='/login'>Sair</Link></li>
+                   </>
                 </ul>
             </div>
         </nav>    
