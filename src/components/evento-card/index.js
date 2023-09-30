d777b4a87ac6390b6b54d8ea7f45b9ccba220487
Tarcio Teles <tarcioapps7@gmail.com>
2023-09-30 19:56:44 -0300

    atualizando a páginacom os eventos criados

--- src/components/evento-card/index.js
+++ src/components/evento-card/index.js
@@ -7,16 +7,19 @@ function EventoCard() {
     return (
         <>
             <div className='col-md-3 col-sm-12'>
-                <img src='' id='banner-evento' className='card-img-top img-cartao' alt='Imagem do Evento'></img>
-
+                <img src='https://via.placeholder.com/500x500' className='card-img-top img-cartao' alt='Imagem do Evento'></img>
                 <div className='card-body'>
                     <h5>Titulo do Evento</h5>
-                    <p className='card-text text-justify'>Detalhes do Evento</p>
-
+                    <p className='card-text text-justify'>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia dolorem tempore autem assumenda est numquam magni corrupti totam illum! Laudantium doloribus est omnis asperiores tempora libero quisquam quod esse a.                    </p>
                     <div className='row rodape-card d-flex align-items-center'>
-                        <Link to='/' className='btn btn-sm btn-detalhes'>+Detalhes</Link>
+                        <div className='col-6'>
+                            <Link to='/' className='btn btn-sm btn-detalhes'>+Detalhes</Link>
+                        </div>
+                        <div className='col-6 text-right'>
+                            <i className='fas fa-eye'></i><span>2019</span>
+                        </div>
                     </div>
-
                 </div>
             </div>
         </>
--- src/view/home/index.js
+++ src/view/home/index.js
@@ -1,19 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './home.css';
 import { Link  } from 'react-router-dom';
 import Navbar from '../../components/navbar';
 import { useSelector } from 'react-redux';
 import EventoCard from '../../components/evento-card/';
-
+import firebase from '../../config/firebase';
 
 function Home() {
+
+    const [eventos, setEventos] = useState([]);
+    let listaeventos = [];
+
+    useEffect(() => {
+        firebase.firestore().collection('eventos').get().then(async (resultado) => {
+            await resultado.docs.forEach(doc => {
+                listaeventos.push({
+                    id: doc.id,
+                    ...doc.data()
+                })
+            })
+
+            setEventos(listaeventos)
+        })
+    })
+
+
     return(
         <>
             <Navbar />
             <h1>Home</h1>
             <h1>{useSelector(state => state.usuarioEmail)}</h1>
             <h1>Logado: {useSelector(state => state.usuarioLogado)}</h1>
-            <EventoCard />
+            <div className='row'>
+                {eventos.map(item => <EventoCard/>) }
+               
+  
+            </div>
         </>
     )
 }
